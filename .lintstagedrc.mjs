93c7817d018fa322d98dc0b4bf6cfe4c74e65d16
黄明富 <212149997@qq.com>
2024-11-21 16:15:41 +0800

    perf: 优化部分结构

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,20 @@
 export default {
-    '*.{ts,tsx,js,jsx,cjs,mjs}': 'eslint --fix',
-    '*.{css,scss}': 'stylelint --fix',
-    '*.{ts,tsx,js,jsx,cjs,mjs,html}': 'prettier --write'
-};
-  
+    '*.{js,jsx,ts,tsx}': [
+        'prettier --cache --ignore-unknown  --write',
+        'eslint --cache --fix'
+    ],
+    '*.{scss,less,styl,html,vue,css}': [
+        'prettier --cache --ignore-unknown --write',
+        'stylelint --fix --allow-empty-input'
+    ],
+    '*.md': ['prettier --cache --ignore-unknown --write'],
+    '*.vue': [
+        'prettier --write',
+        'eslint --cache --fix',
+        'stylelint --fix --allow-empty-input'
+    ],
+    '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
+        'prettier --cache --write--parser json'
+    ],
+    'package.json': ['prettier --cache --write']
+}
